42869b5c6ce775279009fb65422909d2f1051008
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-11-21 09:59:54 +0700

    up fitur chatbot ai gemini

---
+++ src/data/keyWord.js
@@ -0,0 +1,20 @@
+export const keyword = [
+    {
+        name: "Sikat Gigi",
+        description: "Bamboo Toothbrush atau sikat gigi bambu bebas plastik",
+        benefits: "mengurangi sampah plastik, mudah terurai secara alami",
+        price: 45000
+    },
+    {
+        name: "Pasta Gigi",
+        description: "Toothpaste Tablets w/ Fluoride - Mint merupakan alternatif ramah lingkungan dari pasta gigi dalam tube plastik dengan cukup masukkan tablet ke mulut lalu kunyah hingga menjadi pasta gigi",
+        benefits: "melindungi gigi dengan fluoride untuk mencegah kerusakan dan ramah lingkungan mengurangi sampah plastik",
+        price: 50000
+    },
+    {
+        name: "Deterjen",
+        description: "Eco Laundry Powder, deterjen cuci ramah lingkungan yang efektif. Dibuat dengan bahan alami, produk ini bebas dari bahan kimia berbahaya dan pewarna sintetis",
+        benefits: "membersihkan pakaian tanpa merusak alam",
+        price: 75000
+    },
+];
